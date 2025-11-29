1d3a59c70ac5569821745b09117b114842ff6068
artur pulatov <a.r.pulatov@gmail.com>
2025-11-29 21:17:58 +0000

    Learning: Step 2 - Yield CPU with k_sleep(), RGB fade sequence

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,17 +1,15 @@
 // SPDX-FileCopyrightText: Copyright (C) 2025 ARDUINO SA
 // SPDX-License-Identifier: MPL-2.0
 
-// Learning Example: Step 1 - Simple HW PWM Fade on LED3_R
+// Learning Example: Step 2 - Yield CPU with k_sleep()
 
 #include <Arduino_RouterBridge.h>
+#include <zephyr/kernel.h>
 
 void setup() {
-    // LED3 has hardware PWM support
     pinMode(LED3_R, OUTPUT);
     pinMode(LED3_G, OUTPUT);
     pinMode(LED3_B, OUTPUT);
-    
-    // LED4 is digital only
     pinMode(LED4_R, OUTPUT);
     pinMode(LED4_G, OUTPUT);
     pinMode(LED4_B, OUTPUT);
@@ -20,20 +18,39 @@ void setup() {
 }
 
 void loop() {
-    // Simple fade effect on LED3_R using hardware PWM
-    
-    // Fade in (0 -> 255)
-    for (int brightness = 0; brightness <= 255; brightness++) {
-        analogWrite(LED3_R, brightness);
-        delay(5);  // 5ms per step = 1.28 seconds total
+    // Fade RED (HW PWM)
+    for (int i = 0; i <= 255; i++) {
+        analogWrite(LED3_R, i);
+        k_sleep(K_MSEC(5));  // Yields CPU to other threads
+    }
+    for (int i = 255; i >= 0; i--) {
+        analogWrite(LED3_R, i);
+        k_sleep(K_MSEC(5));
     }
     
-    // Fade out (255 -> 0)
-    for (int brightness = 255; brightness >= 0; brightness--) {
-        analogWrite(LED3_R, brightness);
-        delay(5);
+    k_sleep(K_MSEC(500));
+    
+    // Fade GREEN (HW PWM)
+    for (int i = 0; i <= 255; i++) {
+        analogWrite(LED3_G, i);
+        k_sleep(K_MSEC(5));
+    }
+    for (int i = 255; i >= 0; i--) {
+        analogWrite(LED3_G, i);
+        k_sleep(K_MSEC(5));
     }
     
-    // Pause
-    delay(500);
+    k_sleep(K_MSEC(500));
+    
+    // Fade BLUE (HW PWM)
+    for (int i = 0; i <= 255; i++) {
+        analogWrite(LED3_B, i);
+        k_sleep(K_MSEC(5));
+    }
+    for (int i = 255; i >= 0; i--) {
+        analogWrite(LED3_B, i);
+        k_sleep(K_MSEC(5));
+    }
+    
+    k_sleep(K_SECONDS(1));  // Pause 1 second
 }
