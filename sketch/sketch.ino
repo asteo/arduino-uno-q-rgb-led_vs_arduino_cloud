ef86efaa227e89340eb87e6d566b21ea02f17d62
artur pulatov <a.r.pulatov@gmail.com>
2025-11-29 22:07:48 +0000

    Confirmed working: LED3 RGB PWM fade (clean build required)

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,63 +1,18 @@
 // SPDX-FileCopyrightText: Copyright (C) 2025 ARDUINO SA
 // SPDX-License-Identifier: MPL-2.0
 
-// Clean PWM abstraction - C++ with references
+// Fixed: Correct PWM indices for LED3
 
 #include <Arduino_RouterBridge.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/pwm.h>
 
-// ============================================================================
-// PWM LED Abstraction
-// ============================================================================
+// Correct indices: LED3_R=5, LED3_G=6, LED3_B=7
+static const struct pwm_dt_spec pwm_led3_r = PWM_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 5);
+static const struct pwm_dt_spec pwm_led3_g = PWM_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 6);
+static const struct pwm_dt_spec pwm_led3_b = PWM_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 7);
 
-struct pwm_led {
-    const struct pwm_dt_spec spec;
-    
-    inline void set(uint8_t brightness) const {
-        uint32_t pulse = (spec.period * brightness) / 255;
-        pwm_set_dt(&spec, spec.period, pulse);
-    }
-    
-    inline void off() const {
-        pwm_set_dt(&spec, spec.period, 0);
-    }
-    
-    inline void on() const {
-        pwm_set_dt(&spec, spec.period, spec.period);
-    }
-};
-
-// LED3 RGB (Hardware PWM)
-static const pwm_led LED3_PWM_R = { PWM_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 5) };
-static const pwm_led LED3_PWM_G = { PWM_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 6) };
-static const pwm_led LED3_PWM_B = { PWM_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 7) };
-
-// ============================================================================
-// Effects
-// ============================================================================
-
-// Fade in and out
-void fade(const pwm_led& led, uint16_t step_ms = 5) {
-    // Fade in
-    for (uint8_t i = 0; i <= 255; i++) {
-        led.set(i);
-        k_sleep(K_MSEC(step_ms));
-    }
-    
-    // Fade out
-    for (int i = 255; i >= 0; i--) {
-        led.set(i);
-        k_sleep(K_MSEC(step_ms));
-    }
-    
-    led.off();
-}
-
-// ============================================================================
-// LED4 Software Timer
-// ============================================================================
-
+// LED4 timer
 struct k_timer led4_timer;
 int led4_state = 0;
 
@@ -81,10 +36,6 @@ void led4_timer_handler(struct k_timer *timer) {
     led4_state = (led4_state + 1) % 5;
 }
 
-// ============================================================================
-// Setup & Loop
-// ============================================================================
-
 void setup() {
     pinMode(LED4_R, OUTPUT);
     pinMode(LED4_G, OUTPUT);
@@ -97,12 +48,47 @@ void setup() {
 }
 
 void loop() {
-    fade(LED3_PWM_R);
+    uint32_t period = pwm_led3_r.period;
+    
+    // Fade RED
+    for (int i = 0; i <= 255; i++) {
+        uint32_t pulse = (period * i) / 255;
+        pwm_set_dt(&pwm_led3_r, period, pulse);
+        k_sleep(K_MSEC(5));
+    }
+    for (int i = 255; i >= 0; i--) {
+        uint32_t pulse = (period * i) / 255;
+        pwm_set_dt(&pwm_led3_r, period, pulse);
+        k_sleep(K_MSEC(5));
+    }
+    
     k_sleep(K_MSEC(300));
     
-    fade(LED3_PWM_G);
+    // Fade GREEN
+    for (int i = 0; i <= 255; i++) {
+        uint32_t pulse = (period * i) / 255;
+        pwm_set_dt(&pwm_led3_g, period, pulse);
+        k_sleep(K_MSEC(5));
+    }
+    for (int i = 255; i >= 0; i--) {
+        uint32_t pulse = (period * i) / 255;
+        pwm_set_dt(&pwm_led3_g, period, pulse);
+        k_sleep(K_MSEC(5));
+    }
+    
     k_sleep(K_MSEC(300));
     
-    fade(LED3_PWM_B);
+    // Fade BLUE
+    for (int i = 0; i <= 255; i++) {
+        uint32_t pulse = (period * i) / 255;
+        pwm_set_dt(&pwm_led3_b, period, pulse);
+        k_sleep(K_MSEC(5));
+    }
+    for (int i = 255; i >= 0; i--) {
+        uint32_t pulse = (period * i) / 255;
+        pwm_set_dt(&pwm_led3_b, period, pulse);
+        k_sleep(K_MSEC(5));
+    }
+    
     k_sleep(K_MSEC(300));
 }
