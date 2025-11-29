993743c89d62ebb458d98349933313e26a7d23cb
artur pulatov <a.r.pulatov@gmail.com>
2025-11-29 14:09:06 +0000

    Test: RGB test sequence then hue sweep, then ready for Cloud control

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,25 +3,86 @@
 
 #include <Arduino_RouterBridge.h>
 
+int currentHue = 0;
+int currentSat = 0;
+int currentBri = 0;
+bool testComplete = false;
+
+void hsvToRgb(int h, int s, int v, int& r, int& g, int& b) {
+    float S = s / 100.0;
+    float V = v / 100.0;
+    float C = V * S;
+    float X = C * (1 - abs(((h / 60) % 2) - 1));
+    float m = V - C;
+
+    float R, G, B;
+
+    if      (h < 60)  { R = C; G = X; B = 0; }
+    else if (h < 120) { R = X; G = C; B = 0; }
+    else if (h < 180) { R = 0; G = C; B = X; }
+    else if (h < 240) { R = 0; G = X; B = C; }
+    else if (h < 300) { R = X; G = 0; B = C; }
+    else              { R = C; G = 0; B = X; }
+
+    r = (int)((R + m) * 255);
+    g = (int)((G + m) * 255);
+    b = (int)((B + m) * 255);
+}
+
+void setLED(int r, int g, int b) {
+    analogWrite(LED_BUILTIN, r);
+    analogWrite(LED_BUILTIN + 1, g);
+    analogWrite(LED_BUILTIN + 2, b);
+}
+
+void updateLed() {
+    int r, g, b;
+    hsvToRgb(currentHue, currentSat, currentBri, r, g, b);
+    setLED(r, g, b);
+}
+
 void set_hue(int h) {
-    // Test: Just turn RED LED on
-    analogWrite(LED_BUILTIN, 255);
-    analogWrite(LED_BUILTIN + 1, 0);
-    analogWrite(LED_BUILTIN + 2, 0);
+    currentHue = h;
+    updateLed();
 }
 
 void set_sat(int s) {
-    // Test: Just turn GREEN LED on
-    analogWrite(LED_BUILTIN, 0);
-    analogWrite(LED_BUILTIN + 1, 255);
-    analogWrite(LED_BUILTIN + 2, 0);
+    currentSat = s;
+    updateLed();
 }
 
 void set_bri(int b) {
-    // Test: Just turn BLUE LED on
-    analogWrite(LED_BUILTIN, 0);
-    analogWrite(LED_BUILTIN + 1, 0);
-    analogWrite(LED_BUILTIN + 2, 255);
+    currentBri = b;
+    updateLed();
+}
+
+void runTests() {
+    // Test 1: R, G, B with 1 second delay
+    setLED(255, 0, 0);  // RED
+    delay(1000);
+    
+    setLED(0, 255, 0);  // GREEN
+    delay(1000);
+    
+    setLED(0, 0, 255);  // BLUE
+    delay(1000);
+    
+    // Test 2: Hue sweep from red to blue (0 to 240 degrees)
+    // 0.5 second per hue change, smooth transition
+    for (int hue = 0; hue <= 240; hue += 10) {
+        int r, g, b;
+        hsvToRgb(hue, 100, 100, r, g, b);
+        setLED(r, g, b);
+        delay(50);  // 50ms * 24 steps = 1.2 seconds total
+    }
+    
+    delay(500);
+    
+    // Turn off all LEDs
+    setLED(0, 0, 0);
+    delay(500);
+    
+    testComplete = true;
 }
 
 void setup() {
@@ -29,10 +90,8 @@ void setup() {
     pinMode(LED_BUILTIN + 1, OUTPUT);
     pinMode(LED_BUILTIN + 2, OUTPUT);
     
-    // Test at startup: Turn LED WHITE
-    analogWrite(LED_BUILTIN, 255);
-    analogWrite(LED_BUILTIN + 1, 255);
-    analogWrite(LED_BUILTIN + 2, 255);
+    // Run startup tests
+    runTests();
 
     Bridge.begin();
     Bridge.provide("set_hue", set_hue);
