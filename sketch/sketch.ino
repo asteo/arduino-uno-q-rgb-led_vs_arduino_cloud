d673d3f8961e74448057491c71bb899d21c21259
artur pulatov <a.r.pulatov@gmail.com>
2025-11-29 21:32:12 +0000

    Fix: Turn off other colors during LED3 fade

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,42 +1,32 @@
 // SPDX-FileCopyrightText: Copyright (C) 2025 ARDUINO SA
 // SPDX-License-Identifier: MPL-2.0
 
-// Learning Example: Step 3 - Software Timer for LED4
+// Learning Example: Step 3 - Software Timer for LED4 (FIXED)
 
 #include <Arduino_RouterBridge.h>
 #include <zephyr/kernel.h>
 
-// Software timer for LED4
 struct k_timer led4_timer;
-int led4_state = 0;  // 0=R, 1=G, 2=B, 3=W, 4=OFF
+int led4_state = 0;
 
 void led4_timer_handler(struct k_timer *timer) {
-    // Turn off all
     digitalWrite(LED4_R, HIGH);
     digitalWrite(LED4_G, HIGH);
     digitalWrite(LED4_B, HIGH);
     
-    // Cycle through R -> G -> B -> W -> OFF
     switch (led4_state) {
-        case 0:  // RED
-            digitalWrite(LED4_R, LOW);
-            break;
-        case 1:  // GREEN
-            digitalWrite(LED4_G, LOW);
-            break;
-        case 2:  // BLUE
-            digitalWrite(LED4_B, LOW);
-            break;
-        case 3:  // WHITE (all on)
+        case 0: digitalWrite(LED4_R, LOW); break;  // RED
+        case 1: digitalWrite(LED4_G, LOW); break;  // GREEN
+        case 2: digitalWrite(LED4_B, LOW); break;  // BLUE
+        case 3:  // WHITE
             digitalWrite(LED4_R, LOW);
             digitalWrite(LED4_G, LOW);
             digitalWrite(LED4_B, LOW);
             break;
-        case 4:  // OFF (already off)
-            break;
+        case 4: break;  // OFF
     }
     
-    led4_state = (led4_state + 1) % 5;  // Cycle 0-4
+    led4_state = (led4_state + 1) % 5;
 }
 
 void setup() {
@@ -47,7 +37,14 @@ void setup() {
     pinMode(LED4_G, OUTPUT);
     pinMode(LED4_B, OUTPUT);
     
-    // Initialize software timer for LED4 (500ms interval)
+    // Turn off all LEDs initially
+    digitalWrite(LED3_R, HIGH);
+    digitalWrite(LED3_G, HIGH);
+    digitalWrite(LED3_B, HIGH);
+    digitalWrite(LED4_R, HIGH);
+    digitalWrite(LED4_G, HIGH);
+    digitalWrite(LED4_B, HIGH);
+    
     k_timer_init(&led4_timer, led4_timer_handler, NULL);
     k_timer_start(&led4_timer, K_MSEC(500), K_MSEC(500));
     
@@ -55,9 +52,9 @@ void setup() {
 }
 
 void loop() {
-    // LED3: Smooth RGB fade (HW PWM)
-    
-    // Fade RED
+    // Fade RED only
+    analogWrite(LED3_G, 0);  // Green OFF
+    analogWrite(LED3_B, 0);  // Blue OFF
     for (int i = 0; i <= 255; i++) {
         analogWrite(LED3_R, i);
         k_sleep(K_MSEC(5));
@@ -69,7 +66,9 @@ void loop() {
     
     k_sleep(K_MSEC(300));
     
-    // Fade GREEN
+    // Fade GREEN only
+    analogWrite(LED3_R, 0);  // Red OFF
+    analogWrite(LED3_B, 0);  // Blue OFF
     for (int i = 0; i <= 255; i++) {
         analogWrite(LED3_G, i);
         k_sleep(K_MSEC(5));
@@ -81,7 +80,9 @@ void loop() {
     
     k_sleep(K_MSEC(300));
     
-    // Fade BLUE
+    // Fade BLUE only
+    analogWrite(LED3_R, 0);  // Red OFF
+    analogWrite(LED3_G, 0);  // Green OFF
     for (int i = 0; i <= 255; i++) {
         analogWrite(LED3_B, i);
         k_sleep(K_MSEC(5));
