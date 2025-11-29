6eabf797d720035e40aa6c61204a2139234804eb
artur pulatov <a.r.pulatov@gmail.com>
2025-11-29 11:11:44 +0000

    Fix: Set callback before registration and cast to int

--- python/main.py
+++ python/main.py
@@ -7,21 +7,21 @@ from typing import Any
 import time
 
 arduino_cloud = ArduinoCloud()
-cl = ColoredLight("CClight", swi=True)
 
 def light_callback(client: object, value: Any):
     print(f"Cloud updated: {value}")
-
+    print(f"H:{cl.hue} S:{cl.sat} B:{cl.bri}")
+    
     # Send HSV values to MCU via RPC
-    Bridge.call("set_hue", cl.hue)
-    Bridge.call("set_sat", cl.sat)
-    Bridge.call("set_bri", cl.bri)
+    Bridge.call("set_hue", int(cl.hue))
+    Bridge.call("set_sat", int(cl.sat))
+    Bridge.call("set_bri", int(cl.bri))
 
+cl = ColoredLight("CClight", swi=True, on_write=light_callback)
 arduino_cloud.register(cl)
-cl.on_write = light_callback
 
 App.start_brick(arduino_cloud)
 
 while True:
-    print(f"H:{cl.hue} S:{cl.sat} B:{cl.bri}")
+    print(f"Loop: H:{cl.hue} S:{cl.sat} B:{cl.bri}")
     time.sleep(2)
